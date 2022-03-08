a78898c2e712d63cb519e865986daee33e024f76
João Victor Casteliano Zuliani <99493019+jvczuliani@users.noreply.github.com>
2022-03-08 19:35:11 -0300

    Add files via upload

---
+++ advinhacao.c
@@ -0,0 +1,44 @@
+#inclui <stdio.h>
+#inclui <stdlib.h>
+
+int main ()
+{
+    printf("*******************************************\n");
+    printf("BEM VINDO AO JOGO DA ADVINHAÇÃO");
+    printf("*******************************************\n");
+
+    // 1-> Vai listar para o usuário as dificuldades (1)Fácil (2)Moderado (3)Dificil
+    // 2-> LER O VALOR DIGITADO DO USUÁRIO ***********
+
+    // SE FOR FÁCIL GERAMOS UM NÚMERO DE 0-10
+    // SE FOR MODERADO GERAMOS UM ALEATÓRIO DE 0-50
+    // SE FOR DIFICIL GERAMOS UM DE 0-100
+
+    int numeroAleatorio é= rand () % 50+1 ;
+    printf ("%d \n", numeroAleatorio);
+
+    int inputNumber; 
+
+    while (inputNumer != numeroAleatorio)
+
+    {
+        // COLETA O INPUT DO USUARIO
+        printf("Digite um número: \n");
+        scanf("%d", &inputNumber);
+
+        if (inputNumber > numeroAleatorio)
+        {
+            printf("O número que você digitou, é maior do que o esperado.\n");
+        }
+        if (inputNumber < numeroAleatorio)
+        {
+            printf("O número que você digitou, é menor do que o esperado.\n");
+        }
+        if (inputNumber == numeroAleatorio)
+        {
+            printf("Você Ganhou!!!!!\n");            
+        }
+
+    }
+
+}
